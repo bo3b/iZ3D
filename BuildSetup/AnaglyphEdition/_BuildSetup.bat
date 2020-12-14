5f548c895cc7e0b50cfc83ecbf0d1ba267e0770d
bo3b <github@bo3b.net>
2020-12-14 03:07:52 -0800

    Hard code the branch name to 'master' for now.
    This used to fetch the branch name from svn using python of all things.  But, we have git now, so just hard code it for the time being. Revisit.

--- BuildSetup/AnaglyphEdition/_BuildSetup.bat
+++ BuildSetup/AnaglyphEdition/_BuildSetup.bat
@@ -3,10 +3,14 @@
 SET ORIGPATH="%CD%"
 CD %~dp0/../..
 
-set BRANCH_NAME=
-lib\python27\python BuildSetup\SetBranchFolder\SetBranchFolder.py
-call setbranchname.bat
-del setbranchname.bat
+rem this python mechanism was to fetch the branch name as text.
+rem  but we no longer have a svn repo, we have git repo.  For now
+rem  let's just hard code it.  ToDo.
+rem set BRANCH_NAME=
+rem lib\python27\python BuildSetup\SetBranchFolder\SetBranchFolder.py
+rem call setbranchname.bat
+rem del setbranchname.bat
+set BRANCH_NAME=master
 
 cd S3DDriverSetup
 ..\lib\InnoSetup\ISCC.exe SetupScript.iss /dsetupType="Anaglyph" "/dOUTDIR=..\bin\Final Release - Anaglyph" "/dFILESDIR=..\bin\Final Release" /O"..\BuildSetup\AnaglyphEdition\%BRANCH_NAME%"
